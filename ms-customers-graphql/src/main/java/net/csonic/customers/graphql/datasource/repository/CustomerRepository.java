68f126392cd61bb97af7bd6839a6d0a95ee4f81d
manuel <mzegarra@csonic.cloud>
2023-12-23 10:12:16 -0500

    save changes

--- ms-customers-graphql/src/main/java/net/csonic/customers/graphql/datasource/repository/CustomerRepository.java
+++ ms-customers-graphql/src/main/java/net/csonic/customers/graphql/datasource/repository/CustomerRepository.java
@@ -3,7 +3,6 @@ package net.csonic.customers.graphql.datasource.repository;
 
 import net.csonic.customers.graphql.datasource.entity.CustomerEntity;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -16,9 +15,8 @@ import java.util.UUID;
 public interface CustomerRepository extends ListCrudRepository<CustomerEntity, UUID> {
 
     Optional<CustomerEntity> findByDocumentTypeAndDocumentNumber(String documentType, String documentNumber);
+
     //
-    @Query(nativeQuery = true, value = "select * from customers c "
-            + "where (:first_name is null or  upper(first_name) like  '%' || upper(:first_name) || '%') "
-            + "and (:last_name is null or upper(last_name) like  '%' || upper(:last_name) || '%')")
-    List<CustomerEntity> findByKeyword(@Param("first_name") String firstName,@Param("last_name") String lastName);
+    @Query(nativeQuery = true, value = "select * from customers c " + "where (:first_name is null or  upper(first_name) like  '%' || upper(:first_name) || '%') " + "and (:last_name is null or upper(last_name) like  '%' || upper(:last_name) || '%')")
+    List<CustomerEntity> findByKeyword(@Param("first_name") String firstName, @Param("last_name") String lastName);
 }
