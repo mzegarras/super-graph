ff9ba7572fbc709f4dec99c1270fb8c1ed439713
manuel <mzegarra@csonic.cloud>
2024-03-27 23:54:19 -0500

    save changes

--- ms-customers-graphql/src/main/java/net/csonic/customers/graphql/MsCustomersGraphqlApplication.java
+++ ms-customers-graphql/src/main/java/net/csonic/customers/graphql/MsCustomersGraphqlApplication.java
@@ -1,26 +1,32 @@
 package net.csonic.customers.graphql;
 
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.resources.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import io.opentelemetry.exporter.prometheus.PrometheusHttpServer;
+import static io.opentelemetry.semconv.ResourceAttributes.SERVICE_NAME;
+
 
 @SpringBootApplication
 public class MsCustomersGraphqlApplication {
 
 
+    @Bean
+    public OpenTelemetry openTelemetry() {
 
+
+        return AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
+
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MsCustomersGraphqlApplication.class, args);
     }
-/*
-    @Configuration
-    public class InstrumentationConfig {
-        @Bean
-        @ConditionalOnProperty( prefix = "graphql.tracing",
-                name = "enabled", matchIfMissing = true)
-        public Instrumentation tracingInstrumentation(){
-            return new TracingInstrumentation();
-        }
-    }*/
+
 
 }
